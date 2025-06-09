f19b893b916153befdcad88db6bf9a803f9cb227
wongsakorn <wongsakornwinothai@gmail.com>
2025-06-09 13:02:43 +0700

    แก้ console.log database

--- services/db.js
+++ services/db.js
@@ -1,21 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER  , 
-  host: process.env.DB_HOST ,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
-  max: 20, 
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-});
-
-console.log('Connecting to DB with:', {
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
 
 pool.on('connect', () => {
